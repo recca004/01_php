c8ae056277fbd81df554a57ef4438e2cd47741b9
Mario Neita <info@arcalya.com>
2018-02-14 14:18:51 +0100

    added link to home

--- 01_EXOS/view/page.php
+++ 01_EXOS/view/page.php
@@ -7,6 +7,8 @@
     </head>
     <body>
         <div id="page">
+            <a href="<?php echo SITE_URL ; ?>/index.php?page=articles" > Home </a>
+            <a href="<?php echo SITE_URL ; ?>/index.php?page=contact" > Contact </a>
             <main>
                 <?php include SITE_PATH . '/view/'.$datas['view'].'.php'; ?>
             </main>
