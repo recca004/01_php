6ff87dde8e93f854b31e9937625eafe67cd9dee4
PA2 <PA2@ITF5DM.adds>
2018-02-14 14:13:04 +0100

    Blah blah

--- 01_EXOS/index.php
+++ 01_EXOS/index.php
@@ -1,5 +1,5 @@
 <?php
-define ( 'SITE_PATH', realpath( dirname(__FILE__) ) ); 
+define ( 'SITE_PATH', realpath( dirname(__FILE__) ) );
 $site_url = str_replace('\\', '/', str_replace( realpath( $_SERVER[ 'DOCUMENT_ROOT' ] ), '', SITE_PATH ) );
 define ( 'SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] . $site_url );
 
