0d71120bbcddeac6950a69f56b68e9124ba7de0e
PA4 <PA4@ITF5DM.adds>
2018-02-19 13:51:38 +0100

    changes 2

---
+++ includes/commons/ControllerCommon.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ControllerCommon
+ *
+ * @author Mario Neita
+ */
+class ControllerCommon {
+    //put your code here
+
+    protected $_page;
+    protected $_action;
+    protected $_view;
+    protected $_datas;
+        
+    
+    public function __construct($page, $action){
+        $this->_page=$page;
+        $this->_action=$action;
+        $this->_setDatas();
+    }
+    
+    
+    
+    public function get_datas(){
+        return $this->_datas;
+    }
+    public function get_view(){
+        return $this->_view;
+    }
+}
