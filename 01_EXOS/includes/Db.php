02492617ab5cb8f5b2f03fd48470e4681cf8463b
Mario N <info@arcalya.com>
2018-02-20 09:40:23 +0100

    character_set_name

--- 01_EXOS/includes/Db.php
+++ 01_EXOS/includes/Db.php
@@ -11,6 +11,11 @@ Class Db{
 
             self::$_connect = new mysqli( $config['dbhost'], $config['dbuser'], $config['dbpass'], $config['dbname'], $config['dbport'] );
         
+            /* Modification du jeu de résultats en utf8 */
+            //echo self::$_connect->character_set_name();
+            self::$_connect->set_charset("utf8");
+            //echo self::$_connect->character_set_name();
+  
         }
         
         return self::$_connect;
