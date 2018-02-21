ff974586f76e2ad2d8c88196b2314d8e6998b41d
PA3 <PA3@ITF5DM.adds>
2018-02-21 16:57:13 +0100

    add commenters

--- 01_EXOS/includes/Bootstrap.php
+++ 01_EXOS/includes/Bootstrap.php
@@ -12,5 +12,30 @@
  * @author PA3
  */
 class Bootstrap {
+    public static $_page;
+    public static $_action;
+    public static $_router;
+
     //put your code here
+    //lookup page 42 à 45
+    public static function url(){
+        $router= ( isset( $this->_page ) ) ? $this->_page : '';
+
+
+        $url = explode( '/', $urlPath );
+
+        $page   = ( !empty( $url[0] ) ) ? $url[0] : 'articles';
+
+        $action = ( isset( $url[1] ) ) ? $url[1] : '';
+
+        $router = ( isset( $url[2] ) ) ? $url[2] : '';
+
+        if(!file_exists(SITE_PATH . '/application/'.$page.'/Controller.php')){
+            header('HTTP/1.0 404 NOT FOUND');
+            include SITE_PATH . '/view/404.php';
+            exit;
+        }
+        
+    } 
+    
 }
--- 01_EXOS/index.php
+++ 01_EXOS/index.php
@@ -3,29 +3,16 @@ define ( 'SITE_PATH', realpath( dirname(__FILE__) ) );
 $site_url = str_replace('\\', '/', str_replace( realpath( $_SERVER[ 'DOCUMENT_ROOT' ] ), '', SITE_PATH ) );
 define ( 'SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] . $site_url );
 
-$urlPath = ( isset( $_GET['page'] ) ) ? $_GET['page'] : '';
 
 
-$url = explode( '/', $urlPath );
-
-$page   = ( !empty( $url[0] ) ) ? $url[0] : 'articles';
-
-$action = ( isset( $url[1] ) ) ? $url[1] : '';
-
-$router = ( isset( $url[2] ) ) ? $url[2] : '';
-
-if(!file_exists(SITE_PATH . '/application/'.$page.'/Controller.php')){
-    header('HTTP/1.0 404 NOT FOUND');
-    include SITE_PATH . '/view/404.php';
-    exit;
-}
-
+$Bootstrap::url();
 
 include SITE_PATH . '/includes/Db.php';
 include SITE_PATH . '/includes/commons/ControllerCommon.php';
 include SITE_PATH . '/application/'.$page.'/Controller.php';
 
 $Controller = new Controller($page, $action, $router);
+$Bootsrap = new $Bootsrap($page, $action, $router);
 
 $datas = $Controller->get_datas();
 $view = $Controller->get_view();
