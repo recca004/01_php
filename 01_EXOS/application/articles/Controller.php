c33a4064f22b1d40541c03caae1568842e780b31
PA1 <PA1@ITF5DM.adds>
2018-02-23 15:08:32 +0100

    finir controler

--- 01_EXOS/application/articles/Controller.php
+++ 01_EXOS/application/articles/Controller.php
@@ -12,6 +12,7 @@ class Controller extends ControllerCommon{
                 break;
             case 'show':
                 $this->_show();
+                $this->_formUrl();
                  break;
             case 'insert':
                 $this->_insert();
@@ -49,7 +50,7 @@ class Controller extends ControllerCommon{
     private function _article()
     {
         
-        $id = $_GET['id'];
+        $id = $this->_router;
         
         $datas = array();
 
@@ -69,9 +70,9 @@ class Controller extends ControllerCommon{
     
     private function _show()
     {
-        if ( isset( $_GET['id'] ) && !empty( $_GET['id'] ) && is_numeric($_GET['id']) )
+        if ( !empty( $this->_router ) && is_numeric( $this->_router ) )
         {
-            $this->_article( $_GET['id'] );
+            $this->_article( $this->_router );
             
         }
         $this->_view = 'articles/article_form';
@@ -96,7 +97,7 @@ class Controller extends ControllerCommon{
         {
             $this->_view  = 'articles/article_form';
             $this->_datas = $datas;
-            return;
+            return false;
         }
             
         $db = Db::connect();
@@ -112,7 +113,7 @@ class Controller extends ControllerCommon{
         {
             $this->_view  = 'articles/article_form';
             $this->_datas = $datas;
-            return;
+            return false;
         }
 
         $this->_view  = 'articles/articles';
@@ -122,7 +123,7 @@ class Controller extends ControllerCommon{
     private function _del(){
     
         $db = Db::connect();        
-        $id=$db->real_escape_string($_GET['id']);
+        $id=$db->real_escape_string($this->_router);
         $query='DELETE FROM articles WHERE IdArticle ='.$id;
         $db->query($query);
         
@@ -140,7 +141,7 @@ class Controller extends ControllerCommon{
     {
         $datas = $_POST;
         
-        if ( empty( $_GET['id'] ) && !is_numeric($_GET['id']) )
+        if ( empty( $this->_router ) && !is_numeric( $this->_router ) )
         {
             $this->_view = 'articles/article_form';
             $this->_datas = $datas;
@@ -176,7 +177,7 @@ class Controller extends ControllerCommon{
                 . 'TitleArticle = \''.$TitleArticle.'\', '
                 . 'IntroArticle = \''.$IntroArticle.'\', '
                 . 'ContentArticle = \''.$ContentArticle.'\' '
-                . 'WHERE IdArticle = ' . $_GET['id'];
+                . 'WHERE IdArticle = ' . $this->_router;
         
         
         $db->query($query);
@@ -189,15 +190,18 @@ class Controller extends ControllerCommon{
      }
   
      
-     public function get_formUrl()
+     private function _formUrl()
      {
-         if ( isset( $_GET['id'] ) )
+         if ( !empty( $this->_router ) )
          {
-             echo SITE_URL . '/index.php?page=articles&action=update&id=' . $_GET['id'];
+             $this->_datas = $this->_datas['article']->fetch_array();
+             
+             $this->_datas['formUrl'] = SITE_URL . '/articles/update/' . $this->_router;
+         
          }
          else
          {
-             echo SITE_URL . '/index.php?page=articles&action=insert';
+             $this->_datas['formUrl'] = SITE_URL . '/articles/insert';
          }
          
      }
--- 01_EXOS/view/articles/article_detail.php
+++ 01_EXOS/view/articles/article_detail.php
@@ -9,15 +9,15 @@ if( isset( $datas[ 'article' ] ) )
     $row = $datas[ 'article' ]->fetch_array();
     ?>
         <article>
-            <h2><a href="<?php echo SITE_URL; ?>/index.php?page=articles&action=details&id=<?php echo $row[ 'IdArticle' ]; ?>"><?php echo $row[ 'TitleArticle' ]; ?></a></h2>
+            <h2><a href="<?php echo SITE_URL; ?>/articles/detail/<?php echo $row[ 'IdArticle' ]; ?>"><?php echo $row[ 'TitleArticle' ]; ?></a></h2>
             <p>
                 <?php echo $row[ 'IntroArticle' ]; ?>
             </p>
             <p>
                 <?php echo $row[ 'ContentArticle' ]; ?>
             </p>
-<a href="<?php echo SITE_URL; ?>/index.php?page=articles&action=del&id=<?php echo $row[ 'IdArticle' ]; ?>">DEL article</a> 
-  <a href="<?php echo SITE_URL; ?>/index.php?page=articles&action=show&id=<?php echo $row[ 'IdArticle' ]; ?>">show article</a></br>
+            <a href="<?php echo SITE_URL; ?>/articles/del/<?php echo $row[ 'IdArticle' ]; ?>">DEL article</a> 
+            <a href="<?php echo SITE_URL; ?>/articles/show/<?php echo $row[ 'IdArticle' ]; ?>">show article</a></br>
         </article>
     <?php
 }
