af1ca6b6dcb2e732606b4d4413e52c3b7fdfcf13
PA5 <PA5@ITF5DM.adds>
2018-02-23 15:39:44 +0100

    404

--- 01_EXOS/includes/Bootstrap.php
+++ 01_EXOS/includes/Bootstrap.php
@@ -11,27 +11,29 @@
  *
  * @author JV
  */
+class Bootstrap {
 
-class Bootstrap{
-public static $page;
-public static $action;
-public static $router;
-public static function url()
-{
-$router = ( empty( $_GET['page'] ) ) ? 'articles' : $_GET['page'];
-if( !empty( $router ) )
-{
-$parts = explode( '/', $router );
-self::$page = ( isset( $parts[0] ) ) ? $parts[0] : '';
-self::$action = ( isset( $parts[1] ) ) ? $parts[1] : '';
-self::$router = ( isset( $parts[2] ) ) ? $parts[2] : '';
+    public static $page;
+    public static $action;
+    public static $router;
+
+    public static function url()
+    {
+        
+        $urlPath = ( !empty($_GET['page']) ) ? $_GET['page'] : '';
+        
+        $parts = explode('/', $urlPath);
+        
+        self::$page = ( !empty($parts[0]) ) ? $parts[0] : 'articles';
+        self::$action = ( isset($parts[1]) ) ? $parts[1] : '';
+        self::$router = ( isset($parts[2]) ) ? $parts[2] : '';
+            
+        if (!file_exists(SITE_PATH . '/application/' . self::$page .
+                        '/Controller.php')) {
+            header('HTTP/1.0 404 NOT FOUND');
+            include SITE_PATH . '/view/404.php';
+            exit;
+        }
+    }
+
 }
-if( !file_exists( SITE_PATH. '/application/' .self::$page.
-'/Controller.php' ) )
-{
-header('HTTP/1.0 404 NOT FOUND');
-include SITE_PATH . '/view/404.php';
-exit;
-}
-}
-} 
--- 01_EXOS/index.php
+++ 01_EXOS/index.php
@@ -3,7 +3,6 @@ define ( 'SITE_PATH', realpath( dirname(__FILE__) ) );
 $site_url = str_replace('\\', '/', str_replace( realpath( $_SERVER[ 'DOCUMENT_ROOT' ] ), '', SITE_PATH ) );
 define ( 'SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] . $site_url );
 
-
 include SITE_PATH . '/includes/Bootstrap.php';
 
 Bootstrap::url();
@@ -15,8 +14,8 @@ include SITE_PATH . '/application/'.Bootstrap::$page.'/Controller.php';
 
 $Controller = new Controller( Bootstrap::$page, Bootstrap::$action, Bootstrap::$router );
 
-$datas = $Controller->_datas();
-$file_name = $Controller->view();
+$datas = $Controller->get_datas();
+$view = $Controller->get_view();
 
 
 
