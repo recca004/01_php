9acffa7f33a1b299240af585206edf9e9c95c4aa
Mario N <info@arcalya.com>
2018-02-23 15:03:10 +0100

    add Bootstrap

--- 01_EXOS/includes/Bootstrap.php
+++ 01_EXOS/includes/Bootstrap.php
@@ -12,30 +12,26 @@
  * @author PA3
  */
 class Bootstrap {
-    public static $_page;
-    public static $_action;
-    public static $_router;
-
-    //put your code here
-    //lookup page 42 à 45
-    public static function url(){
-        $router= ( isset( $this->_page ) ) ? $this->_page : '';
-
-
-        $url = explode( '/', $urlPath );
-
-        $page   = ( !empty( $url[0] ) ) ? $url[0] : 'articles';
-
-        $action = ( isset( $url[1] ) ) ? $url[1] : '';
-
-        $router = ( isset( $url[2] ) ) ? $url[2] : '';
-
-        if(!file_exists(SITE_PATH . '/application/'.$page.'/Controller.php')){
-            header('HTTP/1.0 404 NOT FOUND');
-            include SITE_PATH . '/view/404.php';
-            exit;
-        }
-        
-    } 
     
+    public static $page;
+    public static $action;
+    public static $router;
+    
+    public static function url(){
+        
+        $router = ( empty( $_GET['page'] ) ) ? 'articles' : $_GET['page'];
+        if( !empty( $router ) ){
+            $parts = explode( '/', $router );
+            self::$page = ( isset( $parts[0] ) ) ? $parts[0] : '';
+            self::$action = ( isset( $parts[1] ) ) ? $parts[1] : '';
+            self::$router = ( isset( $parts[2] ) ) ? $parts[2] : '';
+        }
+        if( !file_exists( SITE_PATH. '/application/' .self::$page.
+        '/Controller.php' ) ){
+        header('HTTP/1.0 404 NOT FOUND');
+        include SITE_PATH . '/view/404.html';
+        exit;
+        }
+    }
+
 }
