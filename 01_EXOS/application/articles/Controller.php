7ae983ca6d533aee34e0dc4b726ef96648bbe9ba
Jacques <olacmoi2000@yahoo.fr>
2018-02-26 14:57:07 +0100

    view

--- 01_EXOS/application/articles/Controller.php
+++ 01_EXOS/application/articles/Controller.php
@@ -1,65 +1,70 @@
 <?php
-
 namespace application\articles;
 
-include_once SITE_PATH .'application/articles/ModelPosts';
+include_once SITE_PATH . '/application/articles/ModelPosts.php';
 
 use application\articles\ModelPosts;
 use includes\commons\ControllerCommon;
 use includes\Db;
-
-class Controller extends ControllerCommon {
-
-    protected function _setDatas() {
+/**
+ * Page article 
+ */
+class Controller extends ControllerCommon{
+    
+    protected function _setDatas(){
         
         $modelPosts = new ModelPosts();
-
-        switch ($this->_action) {
+        
+        switch ($this->_action){
             case 'detail':
-                $this->datas = $modelPosts->article();
-                $this->_view = 'articles/article_detail';
+                $this->_datas = $modelPosts->article();
+                $this->_view  = 'articles/article_detail';
                 break;
             case 'show':
-                $this->datas = $modelPosts->show( $this->_router);
+                $this->_datas = $modelPosts->show( $this->_router );
                 $this->_view = 'articles/article_form';
-                break;
+                 break;
+             
             case 'insert':
                 $datas = $modelPosts->insert();
-                if (empty($datas)) {
-                    $this->_view = 'articles/articles';
-                    $this->_datas = $modelPosts->articles;
-                } else {
-                    $this->_view = 'articles/article_form';
+                if( empty( $datas ) )
+                {
+                    $this->_view  = 'articles/articles';
+                    $this->_datas = $modelPosts->articles();
+                }
+                else
+                {
+                    $this->_view  = 'articles/article_form';
+                    $this->_datas = $datas;
+                }
+                break;
+                 
+            case 'delete':
+                $modelPosts->delete( $this->_router );
+                $this->_datas = $modelPosts->articles();
+                $this->_view  = 'articles/articles';
+                 break;
+             
+            case 'update':
+                $datas = $modelPosts->update( $this->_router );
+                if( empty( $datas ) )
+                {
+                    $this->_view  = 'articles/articles';
+                    $this->_datas = $modelPosts->articles();
+                }
+                else
+                {
+                    $this->_view  = 'articles/article_form';
                     $this->_datas = $datas;
                 }
                 break;
                 
-            case 'delete':
-                $datas = $modelPosts->update( $this->_router );
-                $this->_datas = $modelPosts->articles();
-                $this->_view = 'articles/articles';
-                break;
-            
-            case 'update':
-                $modelPosts->update( $this->_router );
-                 if (empty($datas)) {
-                    $this->_view = 'articles/articles';
-                    $this->_datas = $modelPosts->articles;
-                } else {
-                    $this->_view = 'articles/article_form';
-                    $this->_datas = $datas;
-                }
-                break;
-
-
             default :
-                $modelPosts->articles();
                 $this->_datas = $modelPosts->articles();
-                $this->_view = 'articles/article_detail';
+                $this->_view  = 'articles/articles';
                 break;
         }
     }
 
-    
 
 }
--- 01_EXOS/application/articles/ModelPosts.php
+++ 01_EXOS/application/articles/ModelPosts.php
@@ -1,161 +1,151 @@
 <?php
-namespace applications\articles;
+namespace application\articles;
 
 use includes\Db;
 
 class ModelPosts {
-    public function articles() {
+
+    
+    public function articles()
+    {
         $datas = array();
 
         $db = Db::connect();
 
-        $results = $db->query('SELECT * FROM articles');
+        $results = $db->query( 'SELECT * FROM articles' );
 
-        if (!$db->errno && $results->num_rows > 0) {
-            $datas['articles'] = $results;
+        if( !$db->errno && $results->num_rows > 0 )
+        {
+            $datas[ 'articles' ] = $results;
         }
 
         
-        $this->_datas = $datas;
-    }
-
-    public function article() {
-        $id = $this->_router;
-        $datas = array();
-
-        $db = Db::connect();
-
-        $results = $db->query('SELECT * FROM articles WHERE IdArticle = \'' . $db->real_escape_string($id) . '\'');
-
-        if (!$db->errno && $results->num_rows > 0) {
-            $datas['article'] = $results;
-        }
-
-       
         return $datas;
     }
 
-    public function insert() {
-        $datas = $_POST;
 
-        if (empty($datas['TitleArticle'])) {
-            $datas['error']['titleempty'] = true;
-        }
-
-        if (empty($datas['IntroArticle'])) {
-            $datas['error']['introempty'] = true;
-        }
-
-        if (empty($datas['ContentArticle'])) {
-            $datas['error']['contentempty'] = true;
-        }
-
-        if (isset($datas['error'])) {
-            
-            
-            return $datas;
-        }
-
-        $db = Db::connect();
-
-        $TitleArticle = $db->real_escape_string($datas['TitleArticle']);
-        $IntroArticle = $db->real_escape_string($datas['IntroArticle']);
-        $ContentArticle = $db->real_escape_string($datas['ContentArticle']);
-
-        $query = 'INSERT INTO articles VALUES( NULL, \'' . $TitleArticle . '\', \'' . $IntroArticle . '\', \'' . $ContentArticle . '\' )';
-
-        $db->query($query);
-
-        if ($db->errno) {
-            
-            die('Erreur lors du INSERT');
-        }
-
-
-
-        $this->_articles();
-        
-    }
-
-    public function delete($idArticles) {
-
-
-        $db = Db::connect();
-
-        $id = $db->real_escape_string($this->_router);
-        $query = 'DELETE FROM articles WHERE IdArticle = ' . $id;
-        $db->query($query);
-
-
-       
-        
-    }
-
-    public function update( $id ) {
-
-
-        $datas = $_POST;
-
-        if (empty($id) OR ! is_numeric($id)) {
-            
-            return $datas;
-        }
-
-
-        if (empty($datas['TitleArticle'])) {
-            $datas['error']['titleempty'] = true;
-        }
-
-        if (empty($datas['IntroArticle'])) {
-            $datas['error']['introempty'] = true;
-        }
-
-        if (empty($datas['ContentArticle'])) {
-            $datas['error']['contentempty'] = true;
-        }
-
-        if (isset($datas['error'])) {
-            
-            return $datas;
-        }
-
-        $db = Db::connect();
-
-        $TitleArticle = $db->real_escape_string($datas['TitleArticle']);
-        $IntroArticle = $db->real_escape_string($datas['IntroArticle']);
-        $ContentArticle = $db->real_escape_string($datas['ContentArticle']);
-
-        $query = 'UPDATE articles SET '
-                . 'TitleArticle =\'' . $TitleArticle . '\', '
-                . 'IntroArticle =\'' . $IntroArticle . '\', '
-                . 'ContentArticle =\'' . $ContentArticle . '\' '
-                . 'WHERE IdArticle = \'' . $id . '\' ';
-        $db->query($query);
-
-       
-    }
-
-    public function show( $id )
-{
-$datas = [];
-
-if (!empty( $id ) && is_numeric( $id)) 
+    public function article()
     {
+        $id = $this->_router;
+        
+        $datas = array();
 
-$article = $this->article( $id);
-$datas = $article->fetch_array();
+        $db = Db::connect();
 
-$datas['formUrl'] = SITE_URL . ' /articles/update/' . $id;
+        $results = $db->query( 'SELECT * FROM articles WHERE IdArticle = \''.$db->real_escape_string($id).'\'' );
 
-return $datas;
-}
+        if( !$db->errno && $results->num_rows > 0 )
+        {
+            $datas[ 'article' ] = $results;
+        }
+var_dump($this->_router);
+        return $datas;
+    }
+    
+    
+    public function show( $id )
+    {
+        $datas = [];
+        
+        if ( !empty( $id ) && is_numeric( $id ) )
+        {
+            $article = $this->article( $id );
+            
+            $datas = $article->fetch_array();
 
-else {
-
-$datas['formUrl'] = SITE_URL . ' /articles/insert';
-
-}
-
-return $datas;
-
+            $datas['formUrl'] = SITE_URL . '/articles/update/' . $id;          
+        }
+        else
+        {
+            $datas['formUrl'] = SITE_URL . '/articles/insert';
+        }
+        
+        return $datas;
+    }
+    
+    public function insert()
+    {
+        
+        $datas = $_POST;
+        
+        if (empty(  $datas[ 'TitleArticle' ] ) ){
+            $datas[ 'error' ][ 'titleempty' ] = true;
+        }
+        if (empty( $datas[ 'IntroArticle' ] ) ){
+            $datas[ 'error' ][ 'introempty' ] = true;
+        }
+        if (empty(  $datas[ 'ContentArticle' ] ) ){
+            $datas[ 'error' ][ 'contentempty' ] = true;
+        }
+        
+        if ( isset( $datas[ 'error' ] ) )
+        {
+            return $datas;
+        }
+            
+        $db = Db::connect();
+         
+        $TitleArticle=$db->real_escape_string($datas['TitleArticle']);
+        $IntroArticle=$db->real_escape_string($datas['IntroArticle']);
+        $ContentArticle=$db->real_escape_string($datas['ContentArticle']);
+        
+        $query = 'INSERT INTO articles VALUES(NULL, \''.$TitleArticle.'\', \''.$IntroArticle.'\', \''.$ContentArticle.'\')';
+        $db->query($query);
+        
+        if( $db->errno )
+        {
+            die('Erreur lors du INSERT');
+        }       
+    }
+    
+    
+    public function delete($idArticle){
+    
+        $db = Db::connect();        
+        $id=$db->real_escape_string($idArticle);
+        $query='DELETE FROM articles WHERE IdArticle ='.$id;
+        $db->query($query);
+        
+     }
+     
+    public function update( $id )
+    {
+        $datas = $_POST;
+        
+        if ( empty( $id ) && !is_numeric( $id ) )
+        {
+            return $datas;
+        }
+        
+        if (empty( $datas[ 'TitleArticle' ] ) ){
+            $datas[ 'error' ][ 'titleempty' ] = true;
+        }
+        if (empty( $datas[ 'IntroArticle' ] ) ){
+            $datas[ 'error' ][ 'introempty' ] = true;
+        }
+        if (empty( $datas[ 'ContentArticle' ] ) ){
+            $datas[ 'error' ][ 'contentempty' ] = true;
+        }
+        
+        if ( isset( $datas[ 'error' ] ) )
+        {
+            return $datas;
+        }
+        
+        $db = Db::connect();
+        
+        $TitleArticle = $db->real_escape_string($datas['TitleArticle']);
+        $IntroArticle = $db->real_escape_string($datas['IntroArticle']);
+        $ContentArticle = $db->real_escape_string($datas['ContentArticle']);
+        
+        $query = 'UPDATE articles SET '
+                . 'TitleArticle = \''.$TitleArticle.'\', '
+                . 'IntroArticle = \''.$IntroArticle.'\', '
+                . 'ContentArticle = \''.$ContentArticle.'\' '
+                . 'WHERE IdArticle = ' . $id;
+        
+        
+        $db->query($query);
+     }
+  
 }
