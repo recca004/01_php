7ca05bba65f1fc862ee4a509879dbed8765fa341
recca004 <butmail01@gmail.com>
2018-03-01 21:56:54 +0100

    Changement nécessaire pour show dans contact form
    je tente de faire le changement nécessaire pour régler les différents
    ajouter avec le module contrôleur j'ai réussi à afficher les différents
    informations au moment de tu show mais reste a faire le update et del

--- 01_EXOS/application/articles/Controller.php
+++ 01_EXOS/application/articles/Controller.php
@@ -1,32 +1,34 @@
 <?php
 namespace application\articles;
 
-include_once  SITE_PATH . '/application/articles/ModelPosts.php'; 
+include_once  SITE_PATH . '/application/articles/ModelPosts.php';
 use includes\commons\ControllerCommon;
 use application\articles\ModelPosts;
 //use includes\Db;
 /**
- * Page article 
+ * Page article
  */
 class Controller extends ControllerCommon{
-    
-    
-    
+
+
+
     protected function _setDatas(){
-      $modelPosts=new ModelPosts();  
+      $modelPosts=new ModelPosts();
         switch ($this->_action){
             case 'detail':
                 //$modelPosts->article($this->_router);
-                 $this->_datas = $modelPosts->article($this->_router);
+                $this->_datas = $modelPosts->article($this->_router);
                 $this->_view  = 'articles/article_detail';
                 break;
             case 'show':
+
                 $this->_view = 'articles/article_form';
                 $this->_datas=$modelPosts->show($this->_router);
 
+
                  break;
             case 'insert':
-                $datas=$modelPosts->insert();
+                $this->_datas=$datas=$modelPosts->insert();
                 if (empty($datas)){ $this->_view  = 'articles/articles';
                                     $this->_datas=$modelPosts->articles();
                 }  else {
@@ -36,28 +38,29 @@ class Controller extends ControllerCommon{
                 $modelPosts->insert();
                  break;
             case 'del':
-                $modelPosts->del($this->_router);
+                $this->_datas=$modelPosts->del($this->_router);
                 $this->_datas = $modelPosts->article();
                 $this->_view  = 'articles/articles';
                 break;
             case 'update':
-                $datas=$modelPosts->update($this->_router);
+                $this->_datas=$modelPosts->update($this->_router);
                 if (empty($datas)){ $this->_view  = 'articles/articles';
                                     //$this->_datas=$modelPosts->articles();
                                 $this->_datas = $this->_datas['article']->fetch_array();
                 }  else {
                     $this->_datas = $this->_datas['article']->fetch_array();
 -                   $this->_datas = $datas;
-                                    
+
                 }
                 break;
             default :
                 $modelPosts->articles();
                 $this->_datas = $modelPosts->articles();
                 $this->_view  = 'articles/articles';
-                 
+
                 break;
         }
+
     }
 
 
--- 01_EXOS/application/articles/ModelPosts.php
+++ 01_EXOS/application/articles/ModelPosts.php
@@ -10,7 +10,7 @@ use mysqli;
  */
 class ModelPosts {
 
-    
+
     public function articles()
     {
         $datas = array();
@@ -24,7 +24,7 @@ class ModelPosts {
             $datas[ 'articles' ] = $results;
         }
 
-       
+
         return $datas;
     }
 
@@ -33,7 +33,7 @@ class ModelPosts {
     {
        //$id= $idArticle;
 //        $id = $this->_router;
-        
+
         $datas = array();
 
         $db = Db::connect();
@@ -47,31 +47,52 @@ class ModelPosts {
 
         return $datas;
     }
-    
-    
+
+
     public function show($id)
     {
         $datas=[];
-        
+
         if ( !empty($id) && is_numeric($id) )
         {
-            $article = $this->article($id );
+
             //$article->fetch_array();
-             
+
             $datas['formUrl'] = SITE_URL . '/articles/update/' . $id;
-           
+
+
+
+        $db = Db::connect();
+            $results = $db->query( 'SELECT * FROM articles WHERE IdArticle = \''.$db->real_escape_string($id).'\'' );
+
+            if( !$db->errno && $results->num_rows > 0 )
+            {
+                $datas[ 'article' ] = $results;
+            }
+
+            return $datas;
+var_dump($this->article($id ));
+
+
+
+
+
+
+
+
+
         }else{
             $datas['formUrl'] = SITE_URL . '/articles/insert';
         }
-        
+
         return $datas;
     }
-    
+
     public function insert()
     {
-        
+
         $datas = $_POST;
-        
+
         if (empty(  $datas[ 'TitleArticle' ] ) ){
             $datas[ 'error' ][ 'titleempty' ] = true;
         }
@@ -81,51 +102,50 @@ class ModelPosts {
         if (empty(  $datas[ 'ContentArticle' ] ) ){
             $datas[ 'error' ][ 'contentempty' ] = true;
         }
-        
+
         if ( isset( $datas[ 'error' ] ) )
         {
             return $datas;
         }
-            
+
         $db = Db::connect();
-         
+
         $TitleArticle=$db->real_escape_string($datas['TitleArticle']);
         $IntroArticle=$db->real_escape_string($datas['IntroArticle']);
         $ContentArticle=$db->real_escape_string($datas['ContentArticle']);
-        
+
         $query='INSERT INTO articles VALUES(NULL, \''.$TitleArticle.'\', \''.$IntroArticle.'\', \''.$ContentArticle.'\')';
         $db->query($query);
-        
+
         if( $db->errno )
         {
             die("ERREUR lors du indert");
             return false;
         }
-       
+
     }
     public function del($idArticle){
-    
-        $db = Db::connect();        
+
+        $db = Db::connect();
         $id=$db->real_escape_string($idArticle);
         $query='DELETE FROM articles WHERE IdArticle ='.$id;
         $db->query($query);
 
 
      }
-     
+
     public function update($id)
     {
-        
+
         $datas = $_POST;
-        
+
         if ( empty( $id ) && !is_numeric($id) )
         {
-         
             return $datas;
         }
-        
 
-        
+
+
         if (empty( $datas[ 'TitleArticle' ] ) ){
             $datas[ 'error' ][ 'titleempty' ] = true;
         }
@@ -135,29 +155,29 @@ class ModelPosts {
         if (empty( $datas[ 'ContentArticle' ] ) ){
             $datas[ 'error' ][ 'contentempty' ] = true;
         }
-        
+
         if ( isset( $datas[ 'error' ] ) )
         {
             return $datas;
         }
-        
+
         $db = Db::connect();
-        
+
         $TitleArticle = $db->real_escape_string($datas['TitleArticle']);
         $IntroArticle = $db->real_escape_string($datas['IntroArticle']);
         $ContentArticle = $db->real_escape_string($datas['ContentArticle']);
-        
+
         $query = 'UPDATE articles SET '
                 . 'TitleArticle = \''.$TitleArticle.'\', '
                 . 'IntroArticle = \''.$IntroArticle.'\', '
                 . 'ContentArticle = \''.$ContentArticle.'\' '
                 . 'WHERE IdArticle = ' . $id;
-        
-        
+
+
         $db->query($query);
 
      }
-  
-     
+
+
 
 }
